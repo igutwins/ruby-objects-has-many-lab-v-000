cec756408db5621e608ed5d1c59e88ed8477aae4
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-12 01:01:37 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,8 @@
 class Post
 attr_accessor :title, :author
 @@all = []
-  def initialize(title, author)
+  def initialize(title)
     @title = title
-    @author = author
     @@all << self
   end
   def self.all
