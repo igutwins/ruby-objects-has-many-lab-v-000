eef5e398f071e25906707f127fd0c8d46cb58fbc
Ian Gutwinski <iangutwinski@gmail.com>
2019-03-12 00:22:49 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,7 +9,7 @@ class Artist
   end
   def add_song_by_name(song_name)
     @songs << Song.new
-    song_name = Song.new.name 
+    song_name = Song.new.name
     song.artist = self
   end
-end 
+end
